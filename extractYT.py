914577e4b358956282f3db149e3e954d6e2f98a2
Pmentz <lfants666@gmail.com>
2017-08-21 23:08:33 +0300

    Upgrade to Python3

--- extractYT.py
+++ extractYT.py
@@ -1,17 +1,17 @@
 #f = open("test.html")
 #HTML = f.read()
-import urllib2
-response = urllib2.urlopen('http://www.youtube.com/playlist?list=PL772556F1EFC4D01C')
-HTML = response.read()
+from urllib.request import urlopen
+response = urlopen('http://www.youtube.com/playlist?list=PL772556F1EFC4D01C')
+HTML = response.read().decode('utf-8')
 import re
 
 # http://www.youtube.com/watch?v=3_RhISgoXUs&list=PLED25F943F8D6081C&index=11&feature=plpp_video
 for i in re.findall(r"http://www\.youtube\.com/watch[^\\&]*", HTML):
-  print type(i)
+  print (type(i))
 
 n = 0
 for i in re.findall(r"/watch\?v=[a-zA-Z0-9\-_]+", HTML):
-  print "http://www.youtube.com"+i
+  print ("http://www.youtube.com"+i)
   n = n + 1
 
 import sys
